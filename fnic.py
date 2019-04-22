91efbdce6c484b7b3db4320a966a20fa7cafc335
Halvor W. Sanden <halvor.sanden@gmail.com>
2019-04-22 11:09:07 +0200

    Possible to list each first letter once

--- fnic.py
+++ fnic.py
@@ -2,11 +2,15 @@ import os
 
 path = 'd:\\test\\'
 
-files = []
+files = []  # defines list, empty
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
     for file in f:
-         files.append(file)
+        files.append(file)  # adds every filename (f) to the list
 
-for f in files:
-    print(f)
+letters = []  # new list
+for f in files:  # for every filename
+    letters.append(f[:1])  # add the first letter to the new list
+
+letters = list(dict.fromkeys(letters))  # removes duplicates from new list
+print(letters)
