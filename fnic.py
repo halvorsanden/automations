b253db3ebcc2f110e1ff5b3c02a2b5d53a2b39e4
Halvor W. Sanden <halvor.sanden@gmail.com>
2019-04-22 16:37:27 +0200

    Add conditional result and ignore Thumbs.db

--- fnic.py
+++ fnic.py
@@ -3,19 +3,23 @@ import os
 path = 'd:\\test\\'
 
 files = []  # defines list, empty
+ignored = 'Thumbs.db'
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
     for file in f:
         files.append(file)  # adds every filename (f) to the list
 
+filesclean = files[:]
+filesclean.remove(ignored)
+
 letters = []  # new list
-for f in files:  # for every filename
+for f in filesclean:  # for every filename
     letters.append(f[:1])  # add the first letter to the new list
 
 letters = list(dict.fromkeys(letters))  # removes duplicates from new list
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å']
 
 # make sets with comparisons
 missing = set(alphabet).difference(letters)
@@ -24,7 +28,14 @@ extra = set(letters).difference(alphabet)
 missing = list(missing)
 extra = list(extra)
 
-print("There's room for:")
-print(missing)
-print("There are some extra:")
-print(extra)
+if missing:
+    print("There's room for:")
+    print(missing)
+else:
+    print("All letters are in use.")
+
+if extra:
+    print("Extra prefixes used:")
+    print(extra)
+else:
+    print("No extra prefixes in use.")
