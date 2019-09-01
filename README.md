4e99b672f1246545ef12cff2317280f55f34f970
Halvor William Sanden <halvor.sanden@gmail.com>
2019-09-01 16:57:14 +0200

    Add a semi-working playlist script + update readme
    The script moves and renames files into a new folder.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Checks if prefixes in a given array is used in a selected folder. Can be adapted to any prefix size. Reports unused prefixes and prefixes not in array.
 ## Autofilestamper (afts)
 Auto updates timestamps of linked files in web projects. Can be adapted to use folders and filetypes instead of specific filenames.
+## Playlistmoversorter (poms)
+Moves the files in a playlist into their own folder and number prefixes the files based on the playlist order. Great for podcasts, mixed or dynamic playlists.
---
+++ poms.py
@@ -0,0 +1,85 @@
+# playlistmoversorter
+
+# default mode copies the files
+# run with parameter -m to move files
+# first dialogue asks for playlist
+# second dialogue for parent output folder
+
+import os
+from tkinter.filedialog import askdirectory, askopenfilename
+import ntpath
+import re
+import shutil
+
+playlist = askopenfilename()
+parentPath = askdirectory()
+
+print(playlist)
+
+# set the new folder to the filename part of the playlist path
+playlistName = ntpath.basename(playlist)
+newFolderName = os.path.splitext(playlistName)[0]
+
+print(newFolderName)
+
+newLocation = parentPath + "\\" + newFolderName
+
+# make directory based on playlist name
+if not os.path.exists(newLocation):
+    os.makedirs(newLocation)
+    print("New folder made: " + newLocation)
+else:
+    print("Folder already exists: " + newLocation)
+
+# open/read in the playlist
+with open(playlist, "r", encoding="utf8") as file:
+    dataLines = file.read().splitlines()
+
+trackRegex = r'^[a-zA-Z]:\\(((?![<>:"/\\|?*]).)+((?<![ .])\\)?)*$'
+tracks = []
+
+for line in dataLines:
+    isTrack = re.match(trackRegex, line)
+    if isTrack:
+        # add track name to array
+        tracks.append(line)
+
+print(tracks)
+
+changedTracks = []
+
+for index, line in enumerate(tracks, 1):
+    # get track name from line
+    track = ntpath.basename(line)
+    # setup new track name and location
+    movedTrack = f"{newLocation}\\{index} {track}"
+    # rename and move track
+    shutil.move(line, movedTrack)
+    changedTracks.append(movedTrack)
+
+print(changedTracks)
+
+# update playlist with movedTrack
+# with open(playlist, w, encoding="utf8") as file:
+#     file.write(newLocationRegexorsomething)
+# for line in dataLines:
+# add every filename to the list
+
+
+# with open(playlist, "w", encoding="utf8") as file:
+#     filedataUpdated = file.write()
+
+# missing = []
+# # add missing files to missing files array
+# # number of files missing
+# missingnum = len(missing)
+
+# # number of files sucessfully processed
+# filenum = len(files)
+
+# print(filenum + "files processed")
+# if missing:
+#     print(missingnum + " files can't be found:")
+#     print(missing)
+# else:
+#     print("No missing files.")
