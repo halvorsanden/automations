2c15b7c8cc2675b9682a0dbb33cdf58ecd14bac7
Halvor William Sanden <halvor.sanden@gmail.com>
2019-09-03 18:15:50 +0200

    Add playlist updating and clean up

--- poms.py
+++ poms.py
@@ -14,38 +14,27 @@ import pathlib
 
 playlist = askopenfilename()
 parentPath = askdirectory()
-
-print(playlist)
-
-# set the new folder to the filename part of the playlist path
 playlistName = ntpath.basename(playlist)
 newFolderName = os.path.splitext(playlistName)[0]
-
-print(newFolderName)
-
 newLocation = parentPath + "\\" + newFolderName
 
-# make directory based on playlist name
 if not os.path.exists(newLocation):
     os.makedirs(newLocation)
     print("New folder made: " + newLocation)
 else:
     print("Folder already exists: " + newLocation)
 
-# open/read in the playlist
 with open(playlist, "r", encoding="utf8") as file:
-    dataLines = file.read().splitlines()
+    originLines = file.read().splitlines()
 
 trackRegex = r'^[a-zA-Z]:\\(((?![<>:"/\\|?*]).)+((?<![ .])\\)?)*$'
 tracks = []
 
-for line in dataLines:
+for line in originLines:
     isTrack = re.match(trackRegex, line)
     if isTrack:
         # add track name to array
         tracks.append(line)
-
-print(tracks)
 
 changedTracks = []
 missing = []
@@ -56,27 +45,33 @@ for index, line in enumerate(tracks, 1):
         # setup new track name and location
         movedTrack = f"{newLocation}\\{index} {track}"
         # rename and move track
-        shutil.move(line, movedTrack)
+        #shutil.move(line, movedTrack)
         changedTracks.append(movedTrack)
     else:
         missing.append(line)
 
-print(changedTracks)
+    for index, line in enumerate(originLines):
+        isTrack = re.match(trackRegex, line)
+        if isTrack:
+            # set a variable with the index of that line
+            lineIndex = index
+            # get the filename from that line
+            originTrack = ntpath.basename(line)
+            for newLine in changedTracks:
+                newTrack = ntpath.basename(newLine).split(maxsplit=1)
+                cleanNewTrack = newTrack[1]
+                # run new filename against the filename of the original file
+                if originTrack == cleanNewTrack:
+                    # remove original line at lineIndex
+                    originLines.remove(line)
+                    # insert newLine at lineIndex
+                    originLines.insert(lineIndex, newLine)
 
-# update playlist with movedTrack
-# with open(playlist, w, encoding="utf8") as file:
-#     file.write(newLocationRegexorsomething)
-# for line in dataLines:
-# add every filename to the list
+# update playlist with new line content and empty last line
+with open(playlist, "w", encoding="utf8") as file:
+    file.write('\n'.join(originLines) + '\n')
 
-
-# with open(playlist, "w", encoding="utf8") as file:
-#     filedataUpdated = file.write()
-
-# number of files missing
 missingnum = len(missing)
-
-# number of files sucessfully processed
 filenum = len(changedTracks)
 
 print(str(filenum) + " processed")
